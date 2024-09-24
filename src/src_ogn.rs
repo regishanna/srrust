6bc7d99ae514832862ecbdee13f49159736e8d70
Regis HANNA <regis.hanna@gmail.com>
2024-09-24 16:57:42 +0200

    [OGN] lecture de la chaine xml de trafic depuis le site OGN

--- src/src_ogn.rs
+++ src/src_ogn.rs
@@ -1,3 +1,4 @@
+use ureq;
 use std::thread;
 
 pub struct SrcOgn {}
@@ -17,7 +18,36 @@ impl SrcOgn {
     fn work_thread() {
         let ogn = Self::new();
         loop {
-            
+            let ogn_string = Self::get_ogn_string();
+            println!("Reponse = {}", ogn_string);
+        }
+    }
+
+    fn get_ogn_string() -> String {
+        // tentative de lecture tant que ce n'est pas un succes
+        loop {
+            // on recupere les infos de trafic sur la france
+            let response = match ureq::get("https://live.glidernet.org/lxml.php?a=0\
+                                                           &b=51.3\
+                                                           &c=42.1\
+                                                           &d=8.4\
+                                                           &e=-5.1")
+                                               .call() {
+                Ok(r) => r,
+                _ => {
+                    // TODO temporiser et faire une trace
+                    continue;
+                }
+            };
+
+            // on recupere le body de la reponse sous forme de chaine de caracteres
+            match response.into_string() {
+                Ok(s) => return s,
+                _ => {
+                    // TODO temporiser et faire une trace
+                    continue;
+                }
+            }
         }
     }
 }
