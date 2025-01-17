9b40a6629ff68d4de8e714bce6365eecf5d5cccd
Regis HANNA <regis.hanna@gmail.com>
2025-01-17 14:48:32 +0100

    Make client simulator less aggressive

--- tests/clients_sim.py
+++ tests/clients_sim.py
@@ -1,15 +1,16 @@
 import socket
 import threading
 import argparse
+import time
 
 
-def create_connection(target_ip, target_port):
+def create_connection(id, target_ip, target_port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((target_ip, target_port))
-        print(f"Connected to {target_ip}:{target_port}")
+        print(f"Id {id}: connected to {target_ip}:{target_port}")
 
-        # Send a position message : (lat = 46.344877, long = -1.466214)
+        # Send a position message: (lat = 46.344877, long = -1.466214)
         pos_msg = b"\x00\x08\x02\xc3\x2a\xad\xff\xe9\xa0\x9a"
         sock.sendall(pos_msg)
 
@@ -20,10 +21,10 @@ def create_connection(target_ip, target_port):
                 break
 
         sock.close()
-        print(f"Disconnected to {target_ip}:{target_port}")
+        print(f"Id {id}: disconnected to {target_ip}:{target_port}")
 
     except Exception as e:
-        print(f"Failed to connect to {target_ip}:{target_port} - {e}")
+        print(f"Id {id}: failed to connect to {target_ip}:{target_port} - {e}")
 
 
 def main():
@@ -34,10 +35,12 @@ def main():
     args = parser.parse_args()
 
     threads = []
-    for _ in range(args.connections):
-        thread = threading.Thread(target=create_connection, args=(args.ip, args.port))
+    for i in range(args.connections):
+        thread = threading.Thread(target=create_connection, args=(i, args.ip, args.port))
         threads.append(thread)
         thread.start()
+        if i % 10 == 0:
+            time.sleep(0.2)
 
     for thread in threads:
         thread.join()
